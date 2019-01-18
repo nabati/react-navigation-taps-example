374decd5d80b5a1933919845598150115d795781
Poyan Nabati <poyan.nabati@gmail.com>
2019-01-18 10:17:02 +0200

    Added MapView example with pan handlers

--- App.js
+++ App.js
@@ -1,49 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {View, Text, PanResponder} from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+import MapView from "react-native-maps";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+        this._panResponder = PanResponder.create({
+            // Ask to be the responder:
+            onStartShouldSetPanResponder: (evt, gestureState) => {
+                console.log('onStartShouldSetPanResponder');
+                return true;
+            },
+            onStartShouldSetPanResponderCapture: (evt, gestureState) => {
+                console.log('onStartShouldSetPanResponderCapture');
+                return true;
+            },
+            onMoveShouldSetPanResponder: (evt, gestureState) => {
+                console.log('onMoveShouldSetPanResponder');
+                return true;
+            },
+            onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
+                console.log('onMoveShouldSetPanResponderCapture');
+                return true;
+            },
+
+            onPanResponderGrant: (evt, gestureState) => {
+                // The gesture has started. Show visual feedback so the user knows
+                // what is happening!
+
+                // gestureState.d{x,y} will be set to zero now
+                console.log('onPanResponderGrant');
+            },
+            onPanResponderMove: (evt, gestureState) => {
+                // The most recent move distance is gestureState.move{X,Y}
+
+                // The accumulated gesture distance since becoming responder is
+                // gestureState.d{x,y}
+                console.log('onPanResponderMove');
+            },
+            onPanResponderTerminationRequest: (evt, gestureState) => {
+                console.log('onPanResponderTerminationRequest');
+                return true;
+            },
+            onPanResponderRelease: (evt, gestureState) => {
+                // The user has released all touches while this view is the
+                // responder. This typically means a gesture has succeeded
+                console.log('onPanResponderRelease');
+            },
+            onPanResponderTerminate: (evt, gestureState) => {
+                // Another component has become the responder, so this gesture
+                // should be cancelled
+                console.log('onPanResponderTerminate');
+            },
+            onShouldBlockNativeResponder: (evt, gestureState) => {
+                // Returns whether this component should block native components from becoming the JS
+                // responder. Returns true by default. Is currently only supported on android.
+                console.log('onShouldBlockNativeResponder');
+                return true;
+            },
+        });
+
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <MapView
+                    style={{flex: 1}}
+                    {...this._panResponder.panHandlers}
+                />
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+export default createStackNavigator({
+    Home: {
+        screen: HomeScreen
+    },
 });
